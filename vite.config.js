c650954fa8ef6081fb9f10642ee778f05f51afe1
mohitDOC <mohitverma.diamondore@gmail.com>
2026-06-09 15:58:32 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Binder33_Task/',
+  base: '/binder33-react/',
   plugins: [
     tailwindcss(),
     react(),
